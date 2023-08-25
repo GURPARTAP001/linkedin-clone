345fe000880fb8a029f7df22538addd973a8fe4f
Gurpartap <gurpartap6125@gmail.com>
2023-08-25 14:03:17 +0530

    made more UI using the css

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
+import Feed from './components/Feed';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       {/* sidebar */}
       <Sidebar/>
       {/* feed */}
+      <Feed/>
       {/* widgets */}
     </div>
      
